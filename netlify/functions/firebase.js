464f96051ef683a58bde235337a169470e9983b9
Leo Del Real <delreal.leo@gmail.com>
2021-03-26 15:21:07 -0500

    commit

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -2,12 +2,12 @@ const firebase = require("firebase/app")
 require("firebase/firestore")
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCMzgcMeipoZEguxq44TLu8VXCio97h95Y",
-  authDomain: "kiei-451-f6add.firebaseapp.com",
-  projectId: "kiei-451-f6add",
-  storageBucket: "kiei-451-f6add.appspot.com",
-  messagingSenderId: "866116706947",
-  appId: "1:866116706947:web:86830093f46602905a71b6"
+  apiKey: "AIzaSyABP5yv9TuxjLgXYkverpi4_YR-OqSaTFY",
+  authDomain: "dish-8e9fb.firebaseapp.com",
+  projectId: "dish-8e9fb",
+  storageBucket: "dish-8e9fb.appspot.com",
+  messagingSenderId: "921403644538",
+  appId: "1:921403644538:web:edfd3efdcf750daaccf9c3"
 };
 
 if (!firebase.apps.length) {
